e2426587d34080cd8f9029b9ca247839a68b6fb4
Sven Lehnert <mail@sven-lehnert.de>
2018-04-16 13:24:38 +0100

    new version 1.4.3

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
  * Plugin Name: BuddyForms WooCommerce Form Elements
  * Plugin URI: http://buddyforms.com/downloads/buddyforms-woocommerce-form-elements/
  * Description: This Plugin adds a new section to the BuddyForms Form Builder with all WooCommerce fields to create Product creation forms for the frontend
- * Version: 1.4.2
+ * Version: 1.4.3
  * Author: ThemeKraft
  * Author URI: https://profiles.wordpress.org/svenl77
  * License: GPLv2 or later
