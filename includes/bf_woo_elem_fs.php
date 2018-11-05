66db4b2ced39cfb282dd9a9831c24d2a390924dd
Guillermo Figueroa <gfirem@gmail.com>
2018-11-05 10:55:56 -0300

    * New version. * Preparing  to integrate with other new AddOns. * Fixed the downloadable option. * Fixed the function to hide the tabs marks to not show and select the first available.

--- includes/bf_woo_elem_fs.php
+++ includes/bf_woo_elem_fs.php
@@ -8,120 +8,127 @@
  * @license    GPLv2 or later
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
-class bf_woo_elem_fs {
-	
-	/**
-	 * Instance of this class.
-	 *
-	 * @var object
-	 */
-	protected static $instance = null;
-	
-	public function __construct() {
-		if ( $this->bwfe_fs_is_parent_active_and_loaded() ) {
-			// If parent already included, init add-on.
-			$this->bwfe_fs_init();
-		} else if ( $this->bwfe_fs_is_parent_active() ) {
-			// Init add-on only after the parent is loaded.
-			add_action( 'buddyforms_core_fs_loaded', array( $this, 'bwfe_fs_init' ) );
-		} else {
-			// Even though the parent is not activated, execute add-on for activation / uninstall hooks.
-			$this->bwfe_fs_init();
-		}
-	}
-	
-	public function bwfe_fs_is_parent_active_and_loaded() {
-		// Check if the parent's init SDK method exists.
-		return function_exists( 'buddyforms_core_fs' );
-	}
-	
-	public function bwfe_fs_is_parent_active() {
-		$active_plugins_basenames = get_option( 'active_plugins' );
-		
-		foreach ( $active_plugins_basenames as $plugin_basename ) {
-			if ( 0 === strpos( $plugin_basename, 'buddyforms/' ) ||
-			     0 === strpos( $plugin_basename, 'buddyforms-premium/' )
-			) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public function bwfe_fs_init() {
-		if ( $this->bwfe_fs_is_parent_active_and_loaded() ) {
-			// Init Freemius.
-			$this->bf_woo_elem_fs();
-		}
-	}
-	
-	/**
-	 * @return Freemius
-	 */
-	public static function getFreemius() {
-		global $bf_woo_elem_fs;
-		
-		return $bf_woo_elem_fs;
-	}
-	
-	// Create a helper function for easy SDK access.
-	
-	/**
-	 * Return an instance of this class.
-	 *
-	 * @return object A single instance of this class.
-	 */
-	public static function get_instance() {
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
-		
-		return self::$instance;
-	}
-	
-	public function bf_woo_elem_fs() {
-		global $bf_woo_elem_fs;
-		
-		if ( ! isset( $bf_woo_elem_fs ) ) {
-			// Include Freemius SDK.
-			if ( file_exists( dirname( dirname( __FILE__ ) ) . '/buddyforms/includes/resources/freemius/start.php' ) ) {
-				// Try to load SDK from parent plugin folder.
-				require_once dirname( dirname( __FILE__ ) ) . '/buddyforms/includes/resources/freemius/start.php';
-			} else if ( file_exists( dirname( dirname( __FILE__ ) ) . '/buddyforms-premium/includes/resources/freemius/start.php' ) ) {
-				// Try to load SDK from premium parent plugin folder.
-				require_once dirname( dirname( __FILE__ ) ) . '/buddyforms-premium/includes/resources/freemius/start.php';
-			}
+class bf_woo_elem_fs
+{
+    /**
+     * Instance of this class.
+     *
+     * @var object
+     */
+    protected static $instance = null;
+
+    public function __construct()
+    {
+        if ($this->bwfe_fs_is_parent_active_and_loaded()) {
+            // If parent already included, init add-on.
+            $this->bwfe_fs_init();
+        } elseif ($this->bwfe_fs_is_parent_active()) {
+            // Init add-on only after the parent is loaded.
+            add_action('buddyforms_core_fs_loaded', array($this, 'bwfe_fs_init'));
+        } else {
+            // Even though the parent is not activated, execute add-on for activation / uninstall hooks.
+            $this->bwfe_fs_init();
+        }
+    }
+
+    public function bwfe_fs_is_parent_active_and_loaded()
+    {
+        // Check if the parent's init SDK method exists.
+        return function_exists('buddyforms_core_fs');
+    }
+
+    public function bwfe_fs_is_parent_active()
+    {
+        $active_plugins_basenames = get_option('active_plugins');
+
+        foreach ($active_plugins_basenames as $plugin_basename) {
+            if (strpos($plugin_basename, 'buddyforms/') === 0 ||
+                 strpos($plugin_basename, 'buddyforms-premium/') === 0
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function bwfe_fs_init()
+    {
+        if ($this->bwfe_fs_is_parent_active_and_loaded()) {
+            // Init Freemius.
+            $this->bf_woo_elem_fs();
+        }
+    }
+
+    /**
+     * @return Freemius
+     */
+    public static function getFreemius()
+    {
+        global $bf_woo_elem_fs;
+
+        return $bf_woo_elem_fs;
+    }
+
+    // Create a helper function for easy SDK access.
+
+    /**
+     * Return an instance of this class.
+     *
+     * @return object A single instance of this class.
+     */
+    public static function get_instance()
+    {
+        // If the single instance hasn't been set, set it now.
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function bf_woo_elem_fs()
+    {
+        global $bf_woo_elem_fs;
+
+        if (! isset($bf_woo_elem_fs)) {
+            // Include Freemius SDK.
+            if (file_exists(dirname(dirname(__FILE__)) . '/buddyforms/includes/resources/freemius/start.php')) {
+                // Try to load SDK from parent plugin folder.
+                require_once dirname(dirname(__FILE__)) . '/buddyforms/includes/resources/freemius/start.php';
+            } elseif (file_exists(dirname(dirname(__FILE__)) . '/buddyforms-premium/includes/resources/freemius/start.php')) {
+                // Try to load SDK from premium parent plugin folder.
+                require_once dirname(dirname(__FILE__)) . '/buddyforms-premium/includes/resources/freemius/start.php';
+            }
 
 
-				$bf_woo_elem_fs = fs_dynamic_init( array(
-				'id'                  => '415',
-				'slug'                => 'buddyforms-woocommerce-form-elements',
-				'type'                => 'plugin',
-				'public_key'          => 'pk_10dd57bd7180476f0221961d9d2c9',
-				'is_premium'          => false,
-				// If your addon is a serviceware, set this option to false.
-				'has_premium_version' => true,
-				'has_paid_plans'      => false,
-				'parent'              => array(
-					'id'         => '391',
-					'slug'       => 'buddyforms',
-					'public_key' => 'pk_dea3d8c1c831caf06cfea10c7114c',
-					'name'       => 'BuddyForms',
-				),
-				'menu'                => array(
-					'slug'       => 'edit.php?post_type=buddyforms-woocommerce-form-elements',
-					'first-path' => 'edit.php?post_type=buddyforms&page=buddyforms_welcome_screen',
-					'support'    => false,
-				),
-			) );
-		}
-		
-		return $bf_woo_elem_fs;
-	}
+            $bf_woo_elem_fs = fs_dynamic_init(array(
+                'id' => '415',
+                'slug' => 'buddyforms-woocommerce-form-elements',
+                'type' => 'plugin',
+                'public_key' => 'pk_10dd57bd7180476f0221961d9d2c9',
+                'is_premium' => false,
+                // If your addon is a serviceware, set this option to false.
+                'has_premium_version' => true,
+                'has_paid_plans' => false,
+                'parent' => array(
+                    'id' => '391',
+                    'slug' => 'buddyforms',
+                    'public_key' => 'pk_dea3d8c1c831caf06cfea10c7114c',
+                    'name' => 'BuddyForms',
+                ),
+                'menu' => array(
+                    'slug' => 'edit.php?post_type=buddyforms-woocommerce-form-elements',
+                    'first-path' => 'edit.php?post_type=buddyforms&page=buddyforms_welcome_screen',
+                    'support' => false,
+                ),
+            ));
+        }
+
+        return $bf_woo_elem_fs;
+    }
 }
--- includes/bf_woo_elem_manager.php
+++ includes/bf_woo_elem_manager.php
@@ -8,65 +8,70 @@
  * @license    GPLv2 or later
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
-class bf_woo_elem_manager {
-	
-	protected static $version = '1.4.4';
-	private static $plugin_slug = 'bf_woo_elem';
-	
-	public function __construct() {
-		require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_log.php';
-		new bf_woo_elem_log();
-		try {
-			$this->bf_wc_fe_includes();
-		} catch ( Exception $ex ) {
-			bf_woo_elem_log::log( array(
-				'action'         => get_class( $this ),
-				'object_type'    => bf_woo_elem_manager::get_slug(),
-				'object_subtype' => 'loading_dependency',
-				'object_name'    => $ex->getMessage(),
-			) );
-			
-		}
-	}
-	
-	public function bf_wc_fe_includes() {
-		require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_builder.php';
-		new bf_woo_elem_form_builder();
+class bf_woo_elem_manager
+{
+    protected static $version = '1.4.5';
+
+    private static $plugin_slug = 'bf_woo_elem';
+
+    public function __construct()
+    {
+        require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_log.php';
+        new bf_woo_elem_log();
+        try {
+            $this->bf_wc_fe_includes();
+        } catch (Exception $ex) {
+            bf_woo_elem_log::log(array(
+                'action' => static::class,
+                'object_type' => self::get_slug(),
+                'object_subtype' => 'loading_dependency',
+                'object_name' => $ex->getMessage(),
+            ));
+        }
+    }
+
+    public function bf_wc_fe_includes()
+    {
+        require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_builder.php';
+        new bf_woo_elem_form_builder();
         do_action('buddyforms_bookeable_product_display_element');
-		require_once WC()->plugin_path() . '/includes/admin/class-wc-admin-post-types.php';
-		require_once WC()->plugin_path() . '/includes/admin/meta-boxes/class-wc-meta-box-product-data.php';
-		require_once WC()->plugin_path() . '/includes/admin/meta-boxes/class-wc-meta-box-product-images.php';
-		require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_elements.php';
-		new bf_woo_elem_form_element();
-		require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_elements_save.php';
-		new bf_woo_elem_form_elements_save();
-		
-		if ( ! function_exists( 'woocommerce_wp_text_input' ) ) {
-			include_once( WC()->plugin_path() . '/includes/admin/wc-meta-box-functions.php' );
-		}
-	}
-	
-	public static function get_slug() {
-		return self::$plugin_slug;
-	}
-	
-	static function get_version() {
-		return self::$version;
-	}
-	
-	/**
-	 * @return array
-	 */
-	public static function get_unhandled_tabs() {
-		$unhandled = array();
-		if ( class_exists( 'WC_Vendors' ) ) {
-			$unhandled['commission'] = array( 'label' => 'WC Vendors' );
-		}
-		
-		return apply_filters( 'bf_woo_element_woo_unhandled_tabs', $unhandled );
-	}
+        require_once WC()->plugin_path() . '/includes/admin/class-wc-admin-post-types.php';
+        require_once WC()->plugin_path() . '/includes/admin/meta-boxes/class-wc-meta-box-product-data.php';
+        require_once WC()->plugin_path() . '/includes/admin/meta-boxes/class-wc-meta-box-product-images.php';
+        require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_elements.php';
+        new bf_woo_elem_form_element();
+        require_once BF_WOO_ELEM_INCLUDES_PATH . 'bf_woo_elem_form_elements_save.php';
+        new bf_woo_elem_form_elements_save();
+
+        if (! function_exists('woocommerce_wp_text_input')) {
+            include_once(WC()->plugin_path() . '/includes/admin/wc-meta-box-functions.php');
+        }
+    }
+
+    public static function get_slug()
+    {
+        return self::$plugin_slug;
+    }
+
+    public static function get_version()
+    {
+        return self::$version;
+    }
+
+    /**
+     * @return array
+     */
+    public static function get_unhandled_tabs()
+    {
+        $unhandled = array();
+        if (class_exists('WC_Vendors')) {
+            $unhandled['commission'] = array('label' => 'WC Vendors');
+        }
+
+        return apply_filters('bf_woo_element_woo_unhandled_tabs', $unhandled);
+    }
 }
