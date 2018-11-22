7a84e70ae12646cbf4d063e1da1247318ba9f064
Victor Marin <marin250189@gmail.com>
2018-11-21 22:17:47 -0400

    fix erro when create product

--- includes/bf_woo_elem_form_elements_save.php
+++ includes/bf_woo_elem_form_elements_save.php
@@ -33,7 +33,7 @@ class bf_woo_elem_form_elements_save
 
     public function buddyforms_woocommerce_update_wc_post_meta($post_id=0)
     {
-       
+
         if ($this->bf_wc_save_meta || $this->bf_wc_save_gallery) {
             $post = get_post($post_id);
             $update_post_type = array(
@@ -48,7 +48,6 @@ class bf_woo_elem_form_elements_save
             if ($this->bf_wc_save_meta) {
                 $_POST['_visibility']='visible';
                 WC_Meta_Box_Product_Data::save($post_id, $post);
-                do_action('woocommerce_process_product_meta', $post_id);
                 update_post_meta($post_id, 'woocommerce', $post_id);
             }
 
